b22400722a4826e4437f835671d02e7e7c673bb2
bloggermasterhub <79012517+bloggermasterhub@users.noreply.github.com>
2026-07-26 18:22:38 +0800

    Add files via upload

---
+++ js/sections/sections.js
@@ -0,0 +1,65 @@
+const sections = [
+    "7-CARNATION",
+    "7-CAMIA",
+    "8-RUBY",
+    "9-EINSTEIN",
+    "9-GALILEO"
+];
+
+const input = document.getElementById("gradeSection");
+const suggestions = document.getElementById("gradeSuggestions");
+
+const studentSuggestions =
+    document.getElementById(
+        "studentSuggestions"
+    );
+
+input.addEventListener("focus", () => {
+
+    if (studentSuggestions) {
+        studentSuggestions.style.display =
+            "none";
+    }
+
+});
+
+input.addEventListener("input", function () {
+    const value = this.value.toLowerCase();
+
+    suggestions.innerHTML = "";
+
+    if (value === "") {
+        suggestions.style.display = "none";
+        return;
+    }
+
+    const matches = sections.filter(section =>
+        section.toLowerCase().includes(value)
+    );
+
+    if (matches.length === 0) {
+        suggestions.style.display = "none";
+        return;
+    }
+
+    matches.forEach(match => {
+        const item = document.createElement("div");
+        item.className = "suggestion-item";
+        item.textContent = match;
+
+        item.addEventListener("click", function () {
+            input.value = match;
+            suggestions.style.display = "none";
+        });
+
+        suggestions.appendChild(item);
+    });
+
+    suggestions.style.display = "block";
+});
+
+document.addEventListener("click", function (e) {
+    if (!e.target.closest(".autocomplete")) {
+        suggestions.style.display = "none";
+    }
+});
